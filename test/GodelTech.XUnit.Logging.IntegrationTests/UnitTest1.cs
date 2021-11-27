e7df8de91b2f73f5e76ced3964ea1be7efb4ac34
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-11-27 21:26:57 +0300

    initial solution code

---
+++ test/GodelTech.XUnit.Logging.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GodelTech.XUnit.Logging.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
